9ca070379dd53d6ddd5f22f3cf72d950ee9b9fdb
Yakov Kovalev <yataw@ya.ru>
2019-12-17 09:30:01 +0300

    save current change

--- src/utils.js
+++ src/utils.js
@@ -10,4 +10,32 @@ const utils = {
     }
 };
 
+// TODO обработчик анимации рамки
+/*
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        [
+            document.querySelector('.pie-cont__spinner'),
+            document.querySelector('.pie-cont__filler'),
+            document.querySelector('.pie-cont__mask'),
+        ].forEach(el => {
+            const animationProp = getComputedStyle(el).animation;
+
+            el.style.animation = 'none';
+            void el.offsetWidth;
+            el.style.animation = animationProp;
+
+            // getComputedStyle(el).animation;
+            // el.style.animation = '';
+
+        });
+
+        // document.documentElement.style.setProperty('--game-state', 'paused');
+
+
+        // document.documentElement.style.setProperty('--game-state', 'running');
+    })
+
+*/
+
 export default utils;
